bc97aa6adf9d8408d1bf144ce83a9fe3a1a36810
Cameron Chardukian <camchardukian@gmail.com>
2021-11-18 23:06:25 -0600

    feature/terms-and-conditions

--- src/pages/summary/test/SummaryForm.test.jsx
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -1,11 +1,16 @@
 import { render, screen } from "@testing-library/react";
 import SummaryForm from "../SummaryForm";
 import userEvent from "@testing-library/user-event";
+import {
+  AGREE_TO_TERMS,
+  CONFIRM_ORDER,
+  TERMS_AND_CONDITIONS,
+} from "../../../utils/constants";
 
 test("The button is disabled by default", () => {
   render(<SummaryForm />);
   const button = screen.getByRole("button", {
-    name: "Confirm order",
+    name: CONFIRM_ORDER,
   });
   expect(button).toBeDisabled();
 });
@@ -13,7 +18,7 @@ test("The button is disabled by default", () => {
 test("The checkbox is unchecked by default", () => {
   render(<SummaryForm />);
   const checkbox = screen.getByRole("checkbox", {
-    name: "I agree to the terms and conditions",
+    name: AGREE_TO_TERMS,
   });
   expect(checkbox).not.toBeChecked();
 });
@@ -21,7 +26,7 @@ test("The checkbox is unchecked by default", () => {
 test("The checkbox's checked property is toggled when clicked", () => {
   render(<SummaryForm />);
   const checkbox = screen.getByRole("checkbox", {
-    name: "I agree to the terms and conditions",
+    name: AGREE_TO_TERMS,
   });
   expect(checkbox).not.toBeChecked();
   userEvent.click(checkbox);
@@ -33,14 +38,29 @@ test("The checkbox's checked property is toggled when clicked", () => {
 test("The button is enabled when the checkbox is checked, and disabled when the checkbox is unchecked", () => {
   render(<SummaryForm />);
   const button = screen.getByRole("button", {
-    name: "Confirm order",
+    name: CONFIRM_ORDER,
   });
   const checkbox = screen.getByRole("checkbox", {
-    name: "I agree to the terms and conditions",
+    name: AGREE_TO_TERMS,
   });
   expect(button).toBeDisabled();
   expect(checkbox).not.toBeChecked();
   userEvent.click(checkbox);
   expect(checkbox).toBeChecked();
   expect(button).toBeEnabled();
+});
+
+test("The detailed terms and conditions appear only when the user hovers over the checkbox's label", () => {
+  render(<SummaryForm />);
+  const agreeToTermsLabel = screen.queryByText(AGREE_TO_TERMS);
+  const detailedTermsNull = screen.queryByText(TERMS_AND_CONDITIONS);
+  expect(detailedTermsNull).not.toBeInTheDocument();
+
+  userEvent.hover(agreeToTermsLabel);
+  const detailedTerms = screen.queryByText(TERMS_AND_CONDITIONS);
+  expect(detailedTerms).toBeInTheDocument();
+
+  userEvent.unhover(agreeToTermsLabel);
+  const detailedTermsNullAgain = screen.queryByText(TERMS_AND_CONDITIONS);
+  expect(detailedTermsNullAgain).not.toBeInTheDocument();
 });
---
+++ src/utils/constants.js
@@ -0,0 +1,4 @@
+export const CONFIRM_ORDER = "Confirm order";
+export const AGREE_TO_TERMS = "I agree to the terms and conditions";
+export const TERMS_AND_CONDITIONS =
+  "You should know that if you try to buy ice cream from our store you will not actually receive anything. By using this application you agree to these terms. If you do not agree, cease use of this application immediately.";
