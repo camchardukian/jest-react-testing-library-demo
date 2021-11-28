2d543759da3bce8227631671abec744ee9cbe1b2
Cameron Chardukian <camchardukian@gmail.com>
2021-11-28 17:42:18 -0600

    feature/alert-banner -- Added an alert banner + tests and fixed the url pathName in the ScoopOption and ToppingOption components so that the images would actually render

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -1,9 +1,12 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import AlertBanner from "../common/AlertBanner";
 import ScoopOption from "./ScoopOption";
 import ToppingOption from "./ToppingOption";
+
 export default function Options({ optionType }) {
   const [items, setItems] = useState([]);
+  const [error, setError] = useState(false);
 
   // optionType is 'scoops' or 'toppings'
   useEffect(() => {
@@ -11,9 +14,13 @@ export default function Options({ optionType }) {
       .get(`http://localhost:3030/${optionType}`)
       .then((response) => setItems(response.data))
       .catch((error) => {
-        //   @TODO -- Handle error response
+        setError(true);
       });
   }, [optionType]);
+
+  if (error) {
+    return <AlertBanner />;
+  }
 
   const ItemComponent = optionType === "scoops" ? ScoopOption : ToppingOption;
 
