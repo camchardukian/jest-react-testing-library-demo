c0cb51976d142b40c3fb85d3a0fba84dacaff81e
Cameron Chardukian <camchardukian@gmail.com>
2021-11-28 19:19:26 -0600

    feature/scoops-subtotal-tests

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,3 +4,4 @@ export const TERMS_AND_CONDITIONS =
   "You should know that if you try to buy ice cream from our store you will not actually receive anything. By using this application you agree to these terms. If you do not agree, cease use of this application immediately.";
 export const UNEXPECTED_ERROR =
   "An unexpected error occured. Please try again later.";
+export const SCOOPS_TOTAL_LABEL = "Scoops total: $";
